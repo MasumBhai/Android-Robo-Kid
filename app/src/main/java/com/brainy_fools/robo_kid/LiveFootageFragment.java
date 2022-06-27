3b89b6645fa9e821865a65e113488fa4f23ec5ae
MasumBhai <abdullahmasum6035@gmail.com>
2022-06-27 22:04:36 +0600

    images from firebase storage fetched

--- app/src/main/java/com/brainy_fools/robo_kid/LiveFootageFragment.java
+++ app/src/main/java/com/brainy_fools/robo_kid/LiveFootageFragment.java
@@ -1,12 +1,22 @@
 package com.brainy_fools.robo_kid;
 
+import android.net.Uri;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.ListResult;
+import com.google.firebase.storage.StorageReference;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,6 +27,8 @@ public class LiveFootageFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    View root;
+
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
@@ -59,6 +71,40 @@ public class LiveFootageFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_live_footage, container, false);
+        root = inflater.inflate(R.layout.fragment_live_footage, container, false);
+
+        final ProgressBar progressBar = root.findViewById(R.id.imageProgressBar);
+        final ArrayList<String> imageList = new ArrayList<>();
+        final RecyclerView recyclerView = root.findViewById(R.id.recyclerView);
+        final ImageAdapter adapter = new ImageAdapter(imageList, getContext());
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+//        StorageReference storageReference = FirebaseStorage.getInstance().getReference().child("Images");
+        StorageReference storageReference = FirebaseStorage.getInstance().getReference();
+
+
+        progressBar.setVisibility(View.VISIBLE);
+
+        storageReference.listAll().addOnSuccessListener(new OnSuccessListener<ListResult>() {
+            @Override
+            public void onSuccess(ListResult listResult) {
+                for (StorageReference fileRef : listResult.getItems()) {
+                    fileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(Uri uri) {
+                            imageList.add(uri.toString());
+//                            Log.d("image item uri: ", uri.toString());
+                        }
+                    }).addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(Uri uri) {
+                            recyclerView.setAdapter(adapter);
+                            progressBar.setVisibility(View.GONE);
+                        }
+                    });
+                }
+            }
+        });
+        return root;
     }
 }
